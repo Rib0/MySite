b90a79e134e902c68408c8bbbd75f3c40024ac76
Nikita <nikitsidenk@mail.ru>
2019-02-11 01:27:51 +0300

    recurse function scrollButton

--- JsModules/ScrollButton.js
+++ JsModules/ScrollButton.js
@@ -2,23 +2,19 @@ class ScrollButton {
     constructor () {
         this.scroll = this.scroll.bind(this);
 
-        this.timerId = null;
         this.scrollTargetCoords = document.getElementById("scroll-target").getBoundingClientRect().top + document.documentElement.scrollTop;
         const button = document.getElementById("scroll-button");
         button.addEventListener('click', this.scroll);
     }
 
-    scroll () {
-        this.timerId = setInterval(() => {
-            const currentScroll = document.documentElement.scrollTop;
-            console.log(currentScroll);
-            console.log(this.scrollTargetCoords);
-            if (currentScroll < this.scrollTargetCoords)
-                window.scrollBy(0, 20)
-            else 
-                clearInterval(this.timerId);
-        }, 20)
-    }
+	scroll () {
+		if (window.pageYOffset >= this.scrollTargetCoords) return;
+		setTimeout(() => {
+			window.scrollBy(0, 20);
+			this.scroll();
+		}, 20)
+	}
+    
 }
 
 const scrollButton = new ScrollButton();
